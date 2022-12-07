7015ba26af642adea503d78a896c3ea8f0cf67e1
Victoria Morton <104038505+TorMorton@users.noreply.github.com>
2022-12-06 19:12:49 -0500

    Tuesday Update

--- project2/src/app/app-routing.module.ts
+++ project2/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { FinishedListComponent } from './finished-list/finished-list.component';
 
 const routes: Routes = [];
 
--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { FinishedListComponent } from './finished-list/finished-list.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    ToDoListComponent,
+    FinishedListComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,

37082ad1163f024405ffebd59a29948dea793894
Victoria Morton <104038505+TorMorton@users.noreply.github.com>
2022-12-09 15:06:16 -0500

    update

--- project-2/src/app/tasks/tasks.component.ts
+++ project-2/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
+
+  @Input() taskName: string = '';
+  @Input() creationDate: string = '';
+  @Input() completedDate: string = '';
 
 }

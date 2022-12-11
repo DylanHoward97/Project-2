36f6239bf1c6deaa59b7af2dcd6038e14090c865
Victoria Morton <104038505+TorMorton@users.noreply.github.com>
2022-12-11 17:47:03 -0500

    sunday

--- project-2/src/app/tasks/tasks.component.ts
+++ project-2/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
@@ -10,5 +10,12 @@ export class TasksComponent {
   @Input() taskName: string = '';
   @Input() creationDate: string = '';
   @Input() completedDate: string = '';
+  @Input() location: string = '';
+
+  @Output()listChangeEvent = new EventEmitter<string>();
+
+  changeList(index: number, destination: string) {
+    this.listChangeEvent.emit(destination);
+  }
 
 }
